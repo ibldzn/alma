824a40f29f7a406b28fd09256451dffacdbefd31
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2026-06-07 13:27:31 +0700

    feat(errors): add ErrEndDateCannotBeInTheFuture for better date validation

--- internal/types/errors.go
+++ internal/types/errors.go
@@ -3,9 +3,10 @@ package types
 import "errors"
 
 var (
-	ErrInvalidDateRange         = errors.New("invalid date range: end date cannot be before start date")
-	ErrInvalidDateFormat        = errors.New("invalid date format: expected YYYY-MM-DD")
-	ErrExpectedStructOrMap      = errors.New("invalid type: expected a struct or map")
-	ErrInvalidData              = errors.New("invalid data")
-	ErrUnableToMapDwhToAppModel = errors.New("unable to map DWH model to application model")
+	ErrInvalidDateRange           = errors.New("invalid date range: end date cannot be before start date")
+	ErrInvalidDateFormat          = errors.New("invalid date format: expected YYYY-MM-DD")
+	ErrExpectedStructOrMap        = errors.New("invalid type: expected a struct or map")
+	ErrInvalidData                = errors.New("invalid data")
+	ErrUnableToMapDwhToAppModel   = errors.New("unable to map DWH model to application model")
+	ErrEndDateCannotBeInTheFuture = errors.New("end date cannot be in the future")
 )
