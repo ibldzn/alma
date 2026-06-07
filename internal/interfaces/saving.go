2c14fb28b83461fee180b032e6680f8fdebc22df
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2026-06-07 16:02:01 +0700

    feat(repositories): remove GetSavingSummary method from SavingRepository

--- internal/interfaces/saving.go
+++ internal/interfaces/saving.go
@@ -9,7 +9,6 @@ import (
 type ISavingRepository interface {
 	GetSavingHistory(ctx context.Context, date string) ([]models.Saving, error)
 	UpsertSavings(ctx context.Context, savings []models.Saving) error
-	GetSavingSummary(ctx context.Context, startDate, endDate string) ([]models.SavingSummaryRow, error)
 }
 
 type ISavingService interface {
