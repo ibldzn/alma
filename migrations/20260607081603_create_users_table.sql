7ed7f3dcad7b09dcbea9c3d68f7bd3bf9681bccd
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2026-06-07 15:17:07 +0700

    feat(migrations): create users table with necessary fields and constraints

---
+++ migrations/20260607081603_create_users_table.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    last_login TIMESTAMP NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
